864767a91689159d19110d85391017f1ca19a0ec
lamancha <lamanchakim@gmail.com>
2021-12-26 16:06:49 +0900

    썸네일 관련 기능 추가 1. 라이브러리 추가 2. 이미지 파일일 때 썸네일 추가 3. 파일 관련 객체(AttachFileDTO) 추가 4. json으로 통신

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -3,17 +3,28 @@ package org.zerock.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.domain.AttachFileDTO;
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j
@@ -59,14 +70,28 @@ public class UploadController {
 		return str.replace("-", File.separator);
 	}
 	
-	@PostMapping("/uploadAjaxAction")
-	public void uploacAjaxPost(MultipartFile[] uploadFile) {
-		log.info("update ajax post.........:");
+	private boolean checkImageType(File file) {
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			
+			return contentType.startsWith("image");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+		return false;
+	}
+	
+	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
+		
+		List<AttachFileDTO> list = new ArrayList<>();
 		String uploadFolder = "C:\\upload";
 		
+		String uploadFolderPath = getFolder();
 		// make folder --------
-		File uploadPath = new File(uploadFolder, getFolder());
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
 		log.info("upload path: " + uploadPath);
 		
 		if (uploadPath.exists() == false) {
@@ -75,27 +100,43 @@ public class UploadController {
 		
 		
 		for (MultipartFile multipartFile : uploadFile) {
-			log.info("------------------------------");
-			log.info("Upload File Name: " + multipartFile.getOriginalFilename());
-			log.info("Upload File Size: " + multipartFile.getSize());
+			
+			AttachFileDTO attachDTO = new AttachFileDTO();
 			
 			String uploadFileName = multipartFile.getOriginalFilename();
 			
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
 			
 			log.info("only file name: " + uploadFileName);
+			attachDTO.setFileName(uploadFileName);
 			
 			UUID uuid = UUID.randomUUID();
 			
 			uploadFileName = uuid.toString() + "_" + uploadFileName;
-			
-			File saveFile = new File(uploadFolder, uploadFileName);
-			
+
 			try {
+				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
+				
+				attachDTO.setUuid(uuid.toString());
+				attachDTO.setUploadPath(uploadFolderPath);
+				
+				// check image type file
+				if (checkImageType(saveFile)) {
+					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+					
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+					
+					thumbnail.close();
+				}
+				
+				// add to List
+				list.add(attachDTO);
 			} catch (Exception e) {
-				log.error(e.getMessage());
+				e.printStackTrace();
 			}
 		}
+		
+		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 }
--- src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -59,8 +59,9 @@
 				contentType : false,
 				data : formData,
 				type : "POST",
+				dataType : 'json',
 				success : function(result) {
-					alert("Updated");
+					console.log(result);
 				}
 			});
 		})
